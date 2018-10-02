bd8ac1b7c5045e6b080b14ed72ca1c036bb24607
Petya Djanovska <petya.djanovska@gmail.com>
2018-10-02 14:21:21 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,12 @@
+let pageTemplate =
+   '<div id="page-template"><aside id="sidebar"><div class="post"<p><%= postText %></p><p>Posted By: <span class="author"><%= author %></span></p></div></aside></div>';
+ //create template function
+ let templateFn = _.template(pageTemplat);
+
+
+  function createPost() {
+
+  }
+
+  let postTemplate =
+    '<header class='title'></header><footer class='author'></footer>';
