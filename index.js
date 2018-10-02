72764757fbf82015be564bb5d8d10908754589db
Petya Djanovska <petya.djanovska@gmail.com>
2018-10-02 14:56:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
   function createPost() {
     let pageTemplate = _.template(document.getElementById('page-template').innerHTML);
-    let postTemplate = _document.getElementById('post-template').innerHTML;
+    let postTemplate = _.template(document.getElementById('post-template').innerHTML);
 
     let title = document.getElementById('postTitle').value;
     let content = document.getElementById('postContent').value;
