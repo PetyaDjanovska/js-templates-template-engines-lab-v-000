f8093d69d5c9b8a7c9dced5a57f83a60bc8a19d9
Petya Djanovska <petya.djanovska@gmail.com>
2018-10-02 14:43:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
   function createPost() {
     let title = document.getElementById('postTitle').value;
-    let content = document.getElementById('postContent');
+    let content = document.getElementById('postContent').value;
 
   }
